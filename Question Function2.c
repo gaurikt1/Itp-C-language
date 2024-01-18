b23ee406f7b2a1aa67a8c2e39464cec578ddbd76
gaurikt1 <116288960+gaurikt1@users.noreply.github.com>
2024-01-18 12:55:08 +0530

    Add files via upload

---
+++ Question Function2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int add()
+{
+	int a=1,b=2,sum=0;
+	sum=a+b;
+	printf("sum is:%d",sum);
+	return 0;
+}
+int pro()
+{
+	int a=2,b=4,pro;
+	pro=a*b;
+	printf("pro is:%d",pro);
+	return 0;
+	
+}
+int  main()
+{
+	pro();
+	return 0;
+	
+}
+
